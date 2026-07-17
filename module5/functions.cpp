72c684c10ba0ccd0b1e8754a6aec331f8918ad64
Andy <89361488+Andrex100@users.noreply.github.com>
2026-07-16 20:51:16 -0700

    Create functions.cpp

---
+++ module5/functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+void optionA(int user)
+{
+    cout << user + 10;
+}
+
+void optionB(int user)
+{
+    cout << user / 2;
+}
+
+void optionC(int user)
+{
+    cout << user * 2;
+}
